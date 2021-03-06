678cc8ee915e63b528486db563235f46ede130f1
Pignataro67 <Pignataro67@gmail.com>
2021-03-06 08:00:53 -0800

    adding variables for next project

---
+++ script.js
@@ -0,0 +1,13 @@
+const screens = document.querySelectorAll('.screen');
+const choose_insect_btns = document.querySelectorAll('.choose_insect-btn');
+const start_btn = document.get('.start_btn');
+const game_container = document.getElementById('game_container');
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('score');
+const message = document.getElementById('message');
+
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener()
