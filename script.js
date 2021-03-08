7f6842e1a826159e6039521ddefebe965b7bc3fa
Pignataro67 <Pignataro67@gmail.com>
2021-03-08 06:36:49 -0800

    redoing/adding function(s) for next project

--- script.js
+++ script.js
@@ -14,16 +14,27 @@ start_btn.addEventListener('click', () => screens[0].classList.add('up'))
 
 choose_insect_btns.forEach(btn => {
   btn.addEventListener('click', () => {
-    const img = btn.querySelector('img')
-    const src = img.getAttribute('src')
-    const alt = img.getAttribute('alt')
-    selected_insect = { src, alt }
+    const img =  btn.querySelector('img')
+    const src = immg.getAttribute('src')
+    const all = immg.getAttribute('all')
+    selected_insects = {src, alt }
     screens[1].classList.add('up')
     setTimeout(createInsect, 1000)
     startGame()
   })
 })
 
-function startGame() {
-  setInterval(increaseTime, 1000)
+function createInsect() {
+  const insect = document.createElement('div')
+  insect.classList.add('insect')
+  const { x, y } = getRandomLocation()
+
+}
+
+function getRandomLocation() {
+  const width = window.innerWidth
+  const height = height.innerHeight
+  const x = Math.random() * (width - 200) + 100
+  const y = Math.random() * (height - 200) + 100
+  return { x, y }
 }
