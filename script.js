a57afed747578e802f1db3ae06f7400b4eaf714a
Pignataro67 <Pignataro67@gmail.com>
2021-03-08 07:47:40 -0800

    adding more functions for next project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const screens = document.querySelectorAll('.screen');
-const choose_insect_btns = document.querySelectorAll('.choose_insect-btn');
-const start_btn = document.get('.start_btn');
-const game_container = document.getElementById('game_container');
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn');
+const game_container = document.getElementById('game-container');
 const timeEl = document.getElementById('time');
 const scoreEl = document.getElementById('score');
 const message = document.getElementById('message');
@@ -15,8 +15,8 @@ start_btn.addEventListener('click', () => screens[0].classList.add('up'))
 choose_insect_btns.forEach(btn => {
   btn.addEventListener('click', () => {
     const img =  btn.querySelector('img')
-    const src = immg.getAttribute('src')
-    const all = immg.getAttribute('all')
+    const src = img.getAttribute('src')
+    const alt = img.getAttribute('alt')
     selected_insects = {src, alt }
     screens[1].classList.add('up')
     setTimeout(createInsect, 1000)
@@ -24,17 +24,38 @@ choose_insect_btns.forEach(btn => {
   })
 })
 
+function startGame() {
+  setInterval(increaseTime, 1000)
+}
+
+function increaseTime() {
+  let m = Math.floor(seconds / 60)
+  let s = seconds % 60
+  m = m < 10 ? `0${m}` : m
+  s = s < 10 ? `0${s}` : s
+  timeEl.innerHTML = `Time: ${m}:${s}`
+  seconds++
+}
+
 function createInsect() {
   const insect = document.createElement('div')
   insect.classList.add('insect')
   const { x, y } = getRandomLocation()
+  insect.style.top = `${y}px`
+  insect.style.left = `${x}px`
+  insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" style="transform: rotate(${Math.random() * 360}deg)" />`
 
+  insect.addEventListener('click', catchInsect)
 }
 
 function getRandomLocation() {
   const width = window.innerWidth
-  const height = height.innerHeight
+  const height = window.innerHeight
   const x = Math.random() * (width - 200) + 100
   const y = Math.random() * (height - 200) + 100
   return { x, y }
+}
+
+function catchInsect() {
+  console.log("FUCK YOU")
 }
